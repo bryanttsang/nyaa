f8ad7f90189bd8bf0c8e7fc2bd9d4792f0668b67
bryant <bryant@bryant3.local>
2024-09-07 00:24:31 -0700

    nyaa downloader

--- nyaa/nyaa.py
+++ nyaa/nyaa.py
@@ -1,29 +1,23 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+import requests
+import re
+import time
+import subprocess
 
-f = open("omit.txt", "r")
-omit = f.read().split("\n") # separate titles by a newline
-f.close()
+regex = re.compile(r'<tr class=\"success.+?title=\"\[SubsPlease\] (.+?) \(1080p\).+?href=\"(magnet.+?)\">.+?data-timestamp=\"(\d+?)\">.+?</tr>')
 
-lst = []
+with open('anime.txt', 'r') as f:
+    lines = f.read().splitlines()
+query = '"|"'.join([f'{title.strip()}' for title in lines])
+url = f'https://nyaa.si/user/subsplease?f=0&c=1_2&q=1080p+-batch+("{query}")'
 
-options = Options()
-options.add_argument("--headless")
-driver = webdriver.Chrome(options=options)
-driver.get("https://nyaa.si/user/subsplease?f=0&c=0_0&q=SubsPlease+1080p")
+with open('last_ran_on', 'r+') as f:
+    last_ran_on = int(f.read())
+    f.seek(0)
+    f.write(str(int(time.time())))
 
-for x in range(1, 76):
-    date = driver.find_element_by_xpath("/html/body/div/div/div[1]/table/tbody/tr[%d]/td[5]" %x).get_attribute("title")
-    if "week" in date:
+html = str(requests.get(url).content)
+for title, magnet, timestamp in regex.findall(html):
+    if int(timestamp) < last_ran_on:
         break
-    title = driver.find_element_by_xpath("/html/body/div/div/div[1]/table/tbody/tr[%d]/td[2]/a" %x).text
-    if title.isnumeric():
-        title = driver.find_element_by_xpath("/html/body/div/div/div[1]/table/tbody/tr[%d]/td[2]/a[2]" %x).text
-    if not "Batch" in title and not any(x in title for x in omit):
-        magnet = driver.find_element_by_xpath("/html/body/div/div/div[1]/table/tbody/tr[%d]/td[3]/a[2]" %x).get_attribute("href")
-        lst.append(tuple((title[13:-23], magnet)))
-
-driver.quit()
-
-for t, m in lst:
-    print("\n%s\n%s" %(t.center(80, '='), m))
+    print(title)
+    subprocess.run(["open", magnet])
