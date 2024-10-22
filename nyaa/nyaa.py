1611cf5b8fc653b641181be9f831968055051d84
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2024-10-22 02:33:40 -0700

    prevent redownloading deleted files

--- nyaa/nyaa.py
+++ nyaa/nyaa.py
@@ -1,9 +1,12 @@
 import requests
 import re
 import time
+import pathlib
 import subprocess
 
 regex = re.compile(r'<tr class=\"success.+?title=\"\[SubsPlease\] (.+?) \(1080p\).+?href=\"(magnet.+?)\">.+?data-timestamp=\"(\d+?)\">.+?</tr>')
+
+pathlib.Path('~/Library/Application Support/uTorrent Web/resume.dat').expanduser().unlink(missing_ok=True)
 
 with open('anime.txt', 'r') as f:
     lines = f.read().splitlines()
@@ -20,4 +23,4 @@ for title, magnet, timestamp in regex.findall(html):
     if int(timestamp) < last_ran_on:
         break
     print(title)
-    subprocess.run(["open", magnet.replace('&amp;', '&')])
+    subprocess.run(['open', magnet.replace('&amp;', '&')])
