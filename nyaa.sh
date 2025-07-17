d7a8dd5585e47ff40a7348ae8e05df929b39a11e
bryanttsang <43279760+bryanttsang@users.noreply.github.com>
2025-07-16 20:57:27 -0700

    use multithreading to process multiple sources together

---
+++ nyaa.sh
@@ -0,0 +1,4 @@
+#!/bin/zsh
+
+cd  # path of folder with nyaa.py
+python3 nyaa.py
--- nyaa/nyaa.py
+++ nyaa/nyaa.py
@@ -3,24 +3,39 @@ import re
 import time
 import pathlib
 import subprocess
+import threading
 
-regex = re.compile(r'<tr class=\"success.+?title=\"\[SubsPlease\] (.+?) \(1080p\).+?href=\"(magnet.+?)\">.+?data-timestamp=\"(\d+?)\">.+?</tr>')
-
-pathlib.Path('~/Library/Application Support/uTorrent Web/resume.dat').expanduser().unlink(missing_ok=True)
-
-with open('anime.txt', 'r') as f:
-    lines = f.read().splitlines()
-query = '"|"'.join([f'{title.strip()}' for title in lines if title])
-url = f'https://nyaa.si/user/subsplease?f=0&c=1_2&q=1080p+-batch+("{query}")'
+sources = [
+    {
+        'file': 'subsplease.txt',
+        'query': lambda q: f'https://nyaa.si/user/subsplease?f=0&c=1_2&q=1080p+-batch+("{q}")',
+        'regex': re.compile(r'<tr class=\"success.+?title=\"\[SubsPlease\] (.+?) \(1080p\).+?href=\"(magnet.+?)\">.+?data-timestamp=\"(\d+?)\">.+?</tr>')
+    },
+    {
+        'file': 'erairaws.txt',
+        'query': lambda q: f'https://nyaa.si/user/Erai-raws?f=0&c=1_2&q=1080p+hevc+-batch+("{q}")',
+        'regex': re.compile(r'<tr class=\"success.+?title=\"\[Erai-raws\] (.+?) \[1080p.+?href=\"(magnet.+?)\">.+?data-timestamp=\"(\d+?)\">.+?</tr>')
+    },
+]
 
 with open('last_ran_on', 'r+') as f:
     last_ran_on = int(f.read())
     f.seek(0)
     f.write(str(int(time.time())))
 
-html = str(requests.get(url).content)
-for title, magnet, timestamp in regex.findall(html):
-    if int(timestamp) < last_ran_on:
-        break
-    print(title)
-    subprocess.run(['open', magnet.replace('&amp;', '&')])
+pathlib.Path('~/Library/Application Support/uTorrent Web/resume.dat').expanduser().unlink(missing_ok=True)
+
+def download(file, query, regex):
+    with open(file, 'r') as f:
+        lines = f.read().splitlines()
+    url = query('"|"'.join([f'{title.strip()}' for title in lines if title]))
+    html = str(requests.get(url).content)
+    for title, magnet, timestamp in regex.findall(html):
+        if int(timestamp) < last_ran_on:
+            break
+        print(title)
+        subprocess.run(['open', magnet.replace('&amp;', '&')])
+
+for s in sources:
+    t = threading.Thread(target=download, args=s.values())
+    t.start()
